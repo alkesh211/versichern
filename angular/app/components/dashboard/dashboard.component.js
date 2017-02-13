cc80aba23d76567c42bbde6265e7199571806565
Kamal Joshi <kamal.joshi@iflair.com>
2017-02-13 19:40:38 +0530

    Removed unnecessary layouts

--- angular/app/components/dashboard/dashboard.component.js
+++ angular/app/components/dashboard/dashboard.component.js
@@ -1,19 +1,4 @@
 class DashboardController {
-  constructor ($scope) {
-    'ngInject'
-
-    $scope.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
-    $scope.series = ['Series A', 'Series B']
-    $scope.data = [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
-    ]
-
-    $scope.onClick = function () {}
-
-    $scope.pieLabels = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales']
-    $scope.pieData = [300, 500, 100]
-  }
 }
 
 export const DashboardComponent = {
